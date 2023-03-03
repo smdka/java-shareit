bf99f8d8cee05b4275b51a7e85a3e78621546ad6
smdka <Smdkaalaska@yandex.ru>
2023-03-03 13:14:08 +0300

    <fix> codestyle fix

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -12,13 +12,22 @@ import java.util.Collection;
 @Repository
 public interface BookingRepository extends CrudRepository<Booking, Long>, Serializable {
     Collection<Booking> findAllByBookerId(Long bookerId, Sort sort);
+
     Collection<Booking> findAllByBookerIdAndStatus(Long bookerId, Booking.Status status, Sort sort);
+
     Collection<Booking> findAllByBookerIdAndEndBefore(Long bookerId, LocalDateTime ldt, Sort sort);
+
     Collection<Booking> findAllByBookerIdAndStartAfter(Long bookerId, LocalDateTime ldt, Sort sort);
+
     Collection<Booking> findAllByBookerIdAndStartBeforeAndEndAfter(Long bookerId, LocalDateTime now, LocalDateTime now1, Sort sort);
+
     Collection<Booking> findAllByItemIdIn(Collection<Long> itemIds, Sort sort);
+
     Collection<Booking> findAllByItemIdInAndStatus(Collection<Long> ids, Booking.Status status, Sort sort);
+
     Collection<Booking> findAllByItemIdInAndEndBefore(Collection<Long> ids, LocalDateTime now, Sort sort);
+
     Collection<Booking> findAllByItemIdInAndStartAfter(Collection<Long> ids, LocalDateTime now, Sort sort);
+
     Collection<Booking> findAllByItemIdInAndStartBeforeAndEndAfter(Collection<Long> ids, LocalDateTime now, LocalDateTime now1, Sort sort);
 }
